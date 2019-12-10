e5299fe8d5c4d5bfe14915e1f5ae59048017f347
Jonathan K. Wolski <jonathan.k.wolski@disney.com>
2019-12-10 07:30:44 -0800

    Day 1, part 2

--- 1/part2.rb
+++ 1/part2.rb
@@ -1,6 +1,12 @@
 #!/usr/bin/env ruby
 
+
+def fuel_required_for(amount)
+  n = (amount.to_i / 3.0).floor - 2
+  n.positive? ? n + fuel_required_for(n) : 0
+end
+
 puts ARGF.each_line.map {|n|
-  (n.to_i / 3.0).floor - 2
+  fuel_required_for(n)
 }
 .reduce(0, :+)
