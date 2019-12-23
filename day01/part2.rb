99db3be8607b70b3085d5286aca403514bb8c26a
Jonathan K. Wolski <jonathan.k.wolski@disney.com>
2019-12-23 07:22:26 -0800

    Make recursion tail-recursive for TCO

--- day01/part2.rb
+++ day01/part2.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
 
-def fuel_required_for(amount)
+def fuel_required_for(amount, acc=0)
   n = (amount.to_i / 3.0).floor - 2
-  n.positive? ? n + fuel_required_for(n) : 0
+  n.positive? ? fuel_required_for(n, n + acc) : acc
 end
 
 puts ARGF.each_line.map {|n|
